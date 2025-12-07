7ec2fb692deaad6eaaf9f59c49f04dca89a41330
kaan-w <105508453+kaan-w@users.noreply.github.com>
2025-12-08 00:06:16 +0300

    quit with escape key

--- src/main.c
+++ src/main.c
@@ -62,6 +62,13 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;
     }
+
+    if(event->type == SDL_EVENT_KEY_DOWN) {
+        if(event->key.scancode == SDL_SCANCODE_ESCAPE) {
+            return SDL_APP_SUCCESS;
+        }
+    }
+
     return SDL_APP_CONTINUE;
 }
 
