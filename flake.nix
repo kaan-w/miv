63c64365c8495ecb26fbfc231cbce917a1bd2872
kaan-w <105508453+kaan-w@users.noreply.github.com>
2025-12-10 14:12:22 +0300

    set minimum cmake version to 3.16

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       perSystem = { self', pkgs, lib, ... }: {
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "miv";
-          version = "0.2.0";
+          version = "0.2.1";
           src = lib.cleanSource ./.;
 
           nativeBuildInputs = with pkgs; [
